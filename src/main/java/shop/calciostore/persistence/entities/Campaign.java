20b043a5063d931202ed40e5a9ee5cdf723e8621
Carlos Tiago <carlostiago88@gmail.com>
2018-07-17 21:45:30 -0300

    adding HSQLDB database

--- src/main/java/shop/calciostore/persistence/entities/Campaign.java
+++ src/main/java/shop/calciostore/persistence/entities/Campaign.java
@@ -1,5 +1,7 @@
 package shop.calciostore.persistence.entities;
 
+import javax.persistence.Entity;
 
+@Entity
 public class Campaign {
 }
--- src/main/java/shop/calciostore/restapi/controllers/CampaignController.java
+++ src/main/java/shop/calciostore/restapi/controllers/CampaignController.java
@@ -1,10 +1,8 @@
 package shop.calciostore.restapi.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CampaignController {
-
 
 }
