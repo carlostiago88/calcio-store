a0d3b5740df9e48aa89d2cc3374757590992d8d7
Carlos Tiago <carlostiago88@gmail.com>
2018-07-17 21:04:15 -0300

    adding spring

---
+++ src/main/java/shop/calciostore/restapi/controllers/CustomerController.java
@@ -0,0 +1,7 @@
+package shop.calciostore.restapi.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController {
+}
---
+++ src/test/java/shop/calciostore/CalcioStoreApplicationTests.java
@@ -0,0 +1,16 @@
+package shop.calciostore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalcioStoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
