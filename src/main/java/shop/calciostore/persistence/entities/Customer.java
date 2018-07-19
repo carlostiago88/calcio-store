ea7e077a24f9d4fa619a1f3042f2c03eef0e9e08
Carlos Tiago <carlostiago88@gmail.com>
2018-07-19 10:58:47 -0300

    customer entity created

--- src/main/java/shop/calciostore/persistence/entities/Customer.java
+++ src/main/java/shop/calciostore/persistence/entities/Customer.java
@@ -11,8 +11,43 @@ public class Customer {
     @Id
     @GeneratedValue
     private Long id;
-    private Long soccerTeamId;
+    private SoccerTeam soccerTeam;
     private Date beginDate;
     private Date endDate;
 
+    public Customer() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public SoccerTeam getSoccerTeam() {
+        return soccerTeam;
+    }
+
+    public void setSoccerTeam(SoccerTeam soccerTeam) {
+        this.soccerTeam = soccerTeam;
+    }
+
+    public Date getBeginDate() {
+        return beginDate;
+    }
+
+    public void setBeginDate(Date beginDate) {
+        this.beginDate = beginDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
 }
