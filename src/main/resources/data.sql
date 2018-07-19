ad88a06c061fa0d61abdf9977b39626554b2d0bb
Carlos Tiago <carlostiago88@gmail.com>
2018-07-19 09:41:19 -0300

    creating scripts and entity to teams

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,7 @@
 insert into soccer_team(id,name) values (1,'Corinthians');
+insert into soccer_team(id,name) values (2,'Remo');
+insert into soccer_team(id,name) values (3,'Fluminense');
+insert into soccer_team(id,name) values (4,'Palmeiras');
+insert into soccer_team(id,name) values (5,'Flamengo');
+insert into soccer_team(id,name) values (6,'São Paulo');
+insert into soccer_team(id,name) values (7,'Botafogo');
